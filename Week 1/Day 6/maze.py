6b635a1278c80ece6123b6d0e9061fee1ca8bb0c
alangvazq <alan.g.vazq@gmail.com>
2023-12-01 19:36:36 -0600

    maze

---
+++ Week 1/Day 6/maze.py
@@ -0,0 +1,29 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=problem_world3.json&url=user_world%3Aproblem_world3.json
+
+
+'''def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+ 
+while not at_goal():
+    if wall_on_right() and front_is_clear():
+        move()
+    elif right_is_clear() and front_is_clear():
+        turn_right()
+        if front_is_clear():
+            move()
+            if front_is_clear():
+                move()
+    elif wall_in_front() and wall_on_right():
+        turn_left()
+    elif wall_in_front() and right_is_clear():
+        turn_right()
+        if front_is_clear():
+            move()
+            if front_is_clear() and right_is_clear():
+                turn_left()
+                if front_is_clear():
+                    move()
+        elif front_is_clear() and right_is_clear():
+            turn_right()'''
