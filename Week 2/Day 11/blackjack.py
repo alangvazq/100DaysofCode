edb194f4d1f50208752cd091446ef89842d0ba65
alangvazq <alan.g.vazq@gmail.com>
2023-12-08 22:55:38 -0600

    art

---
+++ Week 2/Day 11/blackjack.py
@@ -0,0 +1,22 @@
+'''import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+computer_cards = []
+play = True
+print("Do you want to play a game of Blackjack? Type 'y' or 'n':")
+
+def add_cards(array):
+  array.append(random.choice(array))
+
+if len(array) < 2:
+  
+  
+
+if play == 'n':
+  play = False
+elif play == 'y':
+  play = True
+
+while play:
+  #print(logo)'''
+  
