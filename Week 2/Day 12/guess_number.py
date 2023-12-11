95d8a21d022b39c6dff9e54d3e4808da779d8227
alangvazq <alan.g.vazq@gmail.com>
2023-12-11 00:16:18 -0600

    v2

--- Week 2/Day 12/guess_number.py
+++ Week 2/Day 12/guess_number.py
@@ -8,15 +8,17 @@
 # If they run out of turns, provide feedback to the player. 
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 import random
+from art import logo
 
 number = list(range(1, 100))
 random = random.choice(number)
 print(random)
 game = True
 
+print(logo)
 print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard':")
+print("\nI'm thinking of a number between 1 and 100.")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
 if difficulty == "easy":
   attempt = 10
@@ -24,22 +26,24 @@ elif difficulty == "hard":
   attempt = 5
 
 while game:
-  print(f"You have {attempt} attempts remaining to guess the number.")
+  print(f"\nYou have {attempt} attempts remaining to guess the number.")
   guess = int(input("Make a guess: "))
 
   if guess > random:
-    print("Too high.")
-    print("Guess again.")
+    print("\nToo high.")
+    
   elif guess < random:
-    print("Too low.")
-    print("Guess again.")
+    print("\nToo low.")
+    
   elif guess == random:
-    print(f"You got it! The answer was {guess}.")
-    game = False
+    print(f"\nYou got it! The answer was {guess} \U0001F604")
+    break
     
   
   attempt -= 1
 
   if attempt == 0:
-    print("You've run out of guesses, you lose.")
-    game = False
+    print("You've run out of guesses, you lose \U0001F622")
+    break
+
+  print("Guess again.")
