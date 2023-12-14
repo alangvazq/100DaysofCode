ccbd1bae68b3ecedfd670f671982b142c99a5038
alangvazq <alan.g.vazq@gmail.com>
2023-12-13 18:03:05 -0600

    Added the coffe-machine project

---
+++ Week_3/Day_16/oop.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+
+# Turtle() = Class
+# timmy = Object
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+
+# Screen() = Class
+# my_screen = Object
+my_screen = Screen()
+
+# my_screen.canvheight = attribute (attributes has, variables)
+print(my_screen.canvheight)
+
+# my_screen.exitonclick() = method (method does, function)
+my_screen.exitonclick()
