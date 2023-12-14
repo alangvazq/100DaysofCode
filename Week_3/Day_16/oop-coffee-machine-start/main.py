e14e8bd47c3d84c117f750c3aa68a4e1eac373d2
alangvazq <alan.g.vazq@gmail.com>
2023-12-13 21:43:34 -0600

    formatting program

--- Week_3/Day_16/oop-coffee-machine-start/main.py
+++ Week_3/Day_16/oop-coffee-machine-start/main.py
@@ -2,10 +2,6 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-drink_1 = MenuItem(name="espresso", cost=1.5, water=50, coffee=18, milk=0)
-drink_2 = MenuItem(name="latte", cost=2.5, water=200, coffee=24, milk=150)
-drink_3 = MenuItem(name="cappuccino", cost=3.0, water=250, coffee=24, milk=100)
-
 drink_info = Menu()
 resources = CoffeeMaker()
 money = MoneyMachine()
@@ -17,10 +13,10 @@ while coffee_maker:
     if choice == "off":
         coffee_maker = False
     elif choice == "report":
-        print(resources.report())
-        print(money.report())
+        resources.report()
+        money.report()
     else:
-        if resources.is_resource_sufficient(drink_info.find_drink(choice)):
-            if money.make_payment(drink_info.find_drink(choice).cost):
-                resources.make_coffee(drink_info.find_drink(choice))
-
+        drink = drink_info.find_drink(choice)
+        if resources.is_resource_sufficient(drink):
+            if money.make_payment(drink.cost):
+                resources.make_coffee(drink)
