d1c92ea9657a9b9292743124d433db92f643f2b1
alangvazq <alan.g.vazq@gmail.com>
2024-01-03 23:37:28 -0600

    Added a file to save the scoreboard

--- Week_3/Day_20_21/scoreboard.py
+++ Week_3/Day_20_21/scoreboard.py
@@ -5,25 +5,33 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.count = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.color("white")
         self.goto(0, 270)
         self.hideturtle()
         self.shapesize(stretch_wid=40, stretch_len=40)
-        self.count = 0
         self.update_score()
 
     def update_score(self):
-        self.write(arg=f"Score: {self.count}", align="center", font=("Arial", 15, "normal"))
+        self.clear()
+        self.write(arg=f"Score: {self.count} High Score: {self.high_score}", align="center",
+                   font=("Arial", 15, "normal"))
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg="GAME OVER", align="center", font=("Arial", 15, "normal"))
+    def reset(self):
+        if self.count > self.high_score:
+            self.high_score = self.count
+            with open("data.txt", "w") as data:
+                data.write(f"{self.high_score}")
+        self.count = 0
+        self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(arg="GAME OVER", align="center", font=("Arial", 15, "normal"))
 
     def print(self):
         self.count += 1
-        self.clear()
         self.update_score()
-
-
-
--- Week_3/Day_20_21/snake.py
+++ Week_3/Day_20_21/snake.py
@@ -24,13 +24,19 @@ class Snake:
             new_segment.goto(position)
             self.segments.append(new_segment)
 
-
     def add_segment(self, position):
         new_segment = Turtle(shape="square")
         new_segment.color("white")
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
