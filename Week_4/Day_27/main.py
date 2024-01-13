492366536a24a7f904c598e40c8dc55430b1471f
alangvazq <alan.g.vazq@gmail.com>
2024-01-13 00:12:13 -0600

    mile to km coverter completedÂ

--- Week_4/Day_27/main.py
+++ Week_4/Day_27/main.py
@@ -3,25 +3,41 @@ from tkinter import *
 
 
 def button_clicked():
-    my_label.config(text=input_entry.get())
+    mile = float(mile_entry.get())
+    km = round(mile * 1.609344, 4)
+    km_entry.delete(0, END)
+    return km_entry.insert(END, string=str(km))
 
 
 window = Tk()
-window.title("My first GUI program")
-window.minsize(width=500, height=300)
-
-# label
-my_label = Label(text="I Am a label", font=("Arial", 24, "bold"))
-my_label.grid(column=0, row=0)
-
-
-# Button
-button = Button(text="Click", command=button_clicked)
-button.grid(column=1, row=1)
-
+window.title("Mile to Km Converter")
+window.config(padx=40, pady=40)
 
 # Entry
-input_entry = Entry(width=10)
-input_entry.grid(column=3, row=2)
+mile_entry = Entry(width=15)
+mile_entry.grid(column=0, row=0)
+
+# label
+equal_label = Label(text="=", font=("Arial", 24, "bold"))
+equal_label.grid(column=1, row=0)
+
+# Entry
+km_entry = Entry(width=15)
+km_entry.grid(column=2, row=0)
+
+# label
+mile_label = Label(text="Mile", font=("Arial", 12))
+mile_label.grid(column=0, row=1)
+mile_label.config(padx=10, pady=10)
+
+# label
+km_label = Label(text="Kilometre", font=("Arial", 12))
+km_label.grid(column=2, row=1)
+km_label.config(padx=10, pady=10)
+
+# Button
+button = Button(text="Calculate", command=button_clicked)
+button.grid(column=1, row=3)
+
 
 tkinter.mainloop()
